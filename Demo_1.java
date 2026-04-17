008612999feee1d821a3d23eeb3a7073d82ebcd3
HAW-Robert <robertarnds1207gmail.com>
2026-04-17 12:21:39 +0200

    Erster Eintrag (Funktion selber geschrieben)

---
+++ Demo_1.java
@@ -0,0 +1,6 @@
+public class Demo_1 {
+    public static void main(String[] args){ //public = jeder hat zugriff | static= Objekt unabhängig 
+        System.out.println("Tschüss Welt");
+    }
+
+} 
